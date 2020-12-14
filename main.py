2282d5e15123b06d64321303194db874a7347104
Mateusz <mati.krzesiak@gmail.com>
2020-12-14 18:14:24 +0100

    functions change 2

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import requests
 import jmespath
 from bs4 import BeautifulSoup
-import re
 
+HTTP_SUCCES = 200
 
 def find_movies():
     response = requests.get('https://www.filmweb.pl/serials/search?endRate=10&orderBy=popularity&descending=true&startRate=9')
-    soup = BeautifulSoup(response.content, 'html.parser')
-    films = soup.find(class_="resultsList hits")
-    return films
+    if response.status_code == HTTP_SUCCES:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        films = soup.find(class_="resultsList hits")
+        if films:
+            return films
+        else:
+            raise Exception("No such an element")
+    else:
+        raise Exception("Invalid response recived")
+
+
+def get_movie_property_text(movie, classname: str) -> str:
+    result = movie.find(class_=classname)
+    return result.get_text()
 
 
 def find_movie_poster(movie):
@@ -16,42 +27,39 @@ def find_movie_poster(movie):
 
 
 def find_movie_title(movie):
-    title = movie.find(class_='filmPreview__title')
-    return title.get_text()
+    return get_movie_property_text(movie, 'filmPreview__title')
 
 
 def find_movie_rate(movie):
-    rate = movie.find(class_='rateBox__rate')
-    return rate.get_text()
+    return get_movie_property_text(movie, 'rateBox__rate')
 
 
 def find_movie_genre(movie):
     try:
-        genre = movie.find(class_='filmPreview__info filmPreview__info--genres')
-        return re.findall('[A-Z][^A-Z]*', genre.get_text())
-    except:
-        return "None"
+        return [genre.get_text() for genre in movie.find(class_='filmPreview__info filmPreview__info--genres').find_all('a')]
+    except AttributeError:
+        return []
 
 
 def find_movie_director(movie):
     try:
-        director = movie.find(class_='filmPreview__info filmPreview__info--directors')
-        return director.get_text().lstrip('twórca')
-    except:
-        return "None"
+        return [director.get_text() for director in movie.find(class_='filmPreview__info filmPreview__info--directors').find_all('a')]
+    except AttributeError:
+        return []
 
 
-def create_dictionaries():
-    final = []
-    for movie in find_movies():
-        result_dic = {
-            'title': find_movie_title(movie),
-            'poster': find_movie_poster(movie),
-            'rate': find_movie_rate(movie),
-            'genre': find_movie_genre(movie),
-            'director': find_movie_director(movie)
-        }
-        final.append(result_dic)
-    return final
+def get_movie(movie):
+    return {
+        'title': find_movie_title(movie),
+        'poster': find_movie_poster(movie),
+        'rate': find_movie_rate(movie),
+        'genre': find_movie_genre(movie),
+        'director': find_movie_director(movie)
+    }
 
-print(create_dictionaries())
+
+def final_results():
+    return [get_movie(movie) for movie in find_movies()]
+
+
+print(final_results())
